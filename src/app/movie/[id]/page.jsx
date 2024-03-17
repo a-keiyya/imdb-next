9aa4bf39fe304267bfe9de1e73c98f103471add4
Keiyya <keiyyaalex@gmail.com>
2024-03-17 10:13:24 +0300

    add movie page

---
+++ src/app/movie/[id]/page.jsx
@@ -0,0 +1,54 @@
+import Image from 'next/image';
+
+export default async function MoviePage({
+	params,
+}) {
+	const movieId = params.id;
+	const res = await fetch(
+		`https://api.themoviedb.org/3/movie/${movieId}?api_key=${process.env.API_KEY}`,
+	);
+
+	const movie = await res.json();
+	//console.log(movie);
+
+	return (
+		<div className='w-full'>
+			<div className='p-4 md:pt-8 flex flex-col md:flex-row content-center max-w-6cl lg:container lg:items-center mx-auto md:space-x-6'>
+				<Image
+					src={`https://image.tmdb.org/t/p/original/${
+						movie.backdrop_path ||
+						movie.poster_path
+					}`}
+					width={500}
+					height={300}
+					className='rounded-lg md:w-1/2'
+					style={{
+						maxWidth: '100%',
+						height: '100%',
+					}}
+				/>
+				<div className='p-2 md:w-1/2'>
+					<h2 className='text-lg lg:text-2xl xl:text-3xl mb-3 font-bold'>
+						{movie.title || movie.name}
+					</h2>
+					<p className='text-lg mb-3'>
+						{movie.overview}
+					</p>
+					<p className='mb-3'>
+						<span className='font-seminold mr-1'>
+							Date Released:
+						</span>
+						{movie.release_date ||
+							movie.first_air_date}
+					</p>
+					<p className='mb-3'>
+						<span className='font-seminold mr-1'>
+							Rating:
+						</span>{' '}
+						{movie.vote_count}
+					</p>
+				</div>
+			</div>
+		</div>
+	);
+}
